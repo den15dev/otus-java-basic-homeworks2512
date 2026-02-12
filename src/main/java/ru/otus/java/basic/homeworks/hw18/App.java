62ac8525c415304ccb4a96ddc4f8e59a0e4eb361
den15 <dendangler@gmail.com>
2026-02-12 20:14:53 +0300

    hw18.02. Create Person database.

--- src/main/java/ru/otus/java/basic/homeworks/hw18/App.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw18/App.java
@@ -13,7 +13,20 @@ public class App {
 
         System.out.println(treeMap);
         System.out.println("Размер TreeMap: " + treeMap.size());
-
         System.out.println("Значение под ключом 4: " + treeMap.get(4));
+
+        PersonDataBase db = new PersonDataBase();
+        db.add(new Person("Иван", Position.DIRECTOR));
+        db.add(new Person("Пётр", Position.ENGINEER));
+        db.add(new Person("Сергей", Position.JUNIOR));
+        db.add(new Person("Андрей", Position.MANAGER));
+        db.add(new Person("Михаил", Position.PLUMBER));
+        db.add(new Person("Константин", Position.SENIOR_MANAGER));
+        db.add(new Person("Евгений", Position.DRIVER));
+
+        System.out.println("Поиск по id 4: " + db.findById(4L));
+        Person person1 = db.findById(4L);
+        System.out.println(person1.getName() + " менеджер? " + db.isManager(person1));
+        System.out.println(db.findById(6L).getName() + " сотрудник? " + db.isEmployee(6L));
     }
 }
